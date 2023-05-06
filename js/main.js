802b75cad64bf6c8b71b115f7ad50d48aba44e9d
Taisei <taisei96.web@gmail.com>
2023-05-06 15:59:00 +0900

    金額処理

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const priceAdd   = document.querySelectorAll('.price-add');
 const memory     = document.querySelectorAll('#memory');
 const storage    = document.querySelectorAll('#storage');
 const priceValue = document.querySelector('.price-value');
@@ -5,6 +6,26 @@ const priceValue = document.querySelector('.price-value');
 let priceFirst  = 208800;
 let addMemory;
 let addStorage;
+
+// for(let i = 0; i < priceAdd.length; i++) {
+//   priceAdd[i].addEventListener('change', ()=> {
+
+//     priceFirst = 208800;
+
+//     for(let indexMemory = 0; i < memory.length; indexMemory++) {
+//         addMemory  = memory[indexMemory].dataset.memory;
+//         console.log(addMemory);
+//     }
+
+//     for(let indexStorage = 0; i < storage.length; indexStorage++) {
+//       addStorage  = storage[indexStorage].dataset.storage;
+//       console.log(addStorage);
+//     }
+
+//     priceFirst = priceFirst + Number(addMemory) + Number(addStorage);
+
+//   })
+// }
 
 for(let i = 0; i < memory.length; i++) {
   memory[i].addEventListener('change', () => {
